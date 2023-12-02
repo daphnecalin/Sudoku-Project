ee18cfad5e8d16ac05bf12f3bc580494437c5beb
Daphne Calin <daphnecalin@gmail.com>
2023-12-02 10:34:43 -0500

    add functions for menu and game loops

--- main.py
+++ main.py
@@ -1,24 +1,63 @@
 import pygame
-from board import Board
+# from board import Board
 from cell import Cell
 from sudoku_generator import SudokuGenerator
 
 pygame.init()
 
-def main():
-    screen = pygame.display.set_mode([512, 512])
-    running = True
+screen = pygame.display.set_mode([512, 512]) # draw things relative to this
+running = True
+is_menu = True
 
-    while running:
+def menu(): # main menu loop
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT:
+            return False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pass
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+    screen.fill((255, 255, 255))
+    titleFont = pygame.font.Font('freesansbold.ttf', 50)
+    titleText = titleFont.render('Sudoku', False, (0, 0, 0))
 
-        screen.fill((255, 255, 255))
+    screen.blit(titleText, (100,100))
+
+    pygame.display.flip()
+    return True # .............
 
 
-    pygame.quit()
+
+def change_to_menu(): # cause menu to be displayed and initialize parts
+    is_menu = True
+    pass
+
+def change_to_game(): # cause game to be displayed and initialize
+    is_menu = False
+    pass
+
+def game(): # main game loop
+    
+    # implement when Board, Cell, SudokuGenerator are finished
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return False
+            
+
+    screen.fill((255, 255, 255))
+
+    
+    pygame.display.flip() # update screen
+
+
+    return True
 
 if __name__ == "__main__":
-    main()
+    while running:
+        
+        if is_menu:
+            running = menu() # cursed
+        else:
+            running = game()
+
+    pygame.quit()
+    
