600be4e2248d7fc4605b76fd813c854d15cea35c
Daphne Calin <daphnecalin@gmail.com>
2023-12-02 12:03:01 -0500

    made menu interface

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ screen = pygame.display.set_mode([512, 512]) # draw things relative to this
 running = True
 is_menu = True
 
+pygame.display.set_caption('Sudoku')
+cat = pygame.image.load('catbox.png')
+bg = pygame.image.load('bg.jpg')
+
+titleFont = pygame.font.Font('freesansbold.ttf', 50)
+smallFont = pygame.font.Font('freesansbold.ttf', 20)
+
+print(pygame.font.get_fonts())
 def menu(): # main menu loop
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT:
@@ -16,11 +24,34 @@ def menu(): # main menu loop
         if event.type == pygame.MOUSEBUTTONDOWN:
             pass
 
+    # Set up screen, window text
     screen.fill((255, 255, 255))
-    titleFont = pygame.font.Font('freesansbold.ttf', 50)
     titleText = titleFont.render('Sudoku', False, (0, 0, 0))
 
-    screen.blit(titleText, (100,100))
+    # Draw title text, background, and image
+    screen.blit(bg, (0,0))
+    pygame.draw.rect(screen, (255,255,255), pygame.Rect(110,80,290,250)) # hard-coded positions ..
+    screen.blit(titleText, (160,100))
+    screen.blit(cat, (195, 150))
+
+    # Draw buttons and get click
+    mouse = pygame.mouse.get_pos() 
+    
+    btnWidth = 95
+    btnHeight = 50
+    
+    pygame.draw.rect(screen, (150, 150, 150), pygame.Rect(205, 325, btnWidth, btnHeight)) # middle button
+    pygame.draw.rect(screen, (125, 125, 125), pygame.Rect(110, 325, btnWidth, btnHeight)) # left
+    pygame.draw.rect(screen, (125, 125, 125), pygame.Rect(300, 325, btnWidth, btnHeight)) # right
+    
+    easyButton = smallFont.render('Easy', False, (0, 0, 0))
+    mediumButton = smallFont.render('Medium', False, (0, 0, 0))
+    hardButton = smallFont.render('Hard', False, (0, 0, 0))
+    
+    
+    screen.blit(easyButton, (130, 340)) # offset of (20, 15)
+    screen.blit(mediumButton, (216, 340)) # offset of (15, 15)
+    screen.blit(hardButton, (320, 340)) # offset of (20, 15)
 
     pygame.display.flip()
     return True # .............
